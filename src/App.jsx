a32868dab730ed748d7b0e6d2641352312b92190
Jordan <74591126+jordansmrd@users.noreply.github.com>
2024-03-28 10:01:48 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,17 @@ function App() {
   return (
     <>
       <h1>Hello ini test deployment with vercel</h1>
+      <h2>test perubahan</h2>
     </>
   );
 }
 
 export default App;
+
+//server => vps, hostinger
+//domain => alamat
+
+//server bisa diakses lewat ip
+
+// 128.0.0.1:8000
+// myproject.com
